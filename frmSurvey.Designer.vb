2f8f807128cfaeb9a720fdee50753358a1a24653
Neina Cichon <ncichon@cincinnatistate.edu>
2021-06-24 18:59:59 -0400

    program

---
+++ frmSurvey.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmHouseholdIncome
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cboCountyState = New System.Windows.Forms.ComboBox()
+        Me.txtYearlyIncome = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnTotalSurveyed = New System.Windows.Forms.Button()
+        Me.btnAverageIncome = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cboCountyState
+        '
+        Me.cboCountyState.FormattingEnabled = True
+        Me.cboCountyState.Items.AddRange(New Object() {"", "Hamilton, OH", "Butler, OH", "Clermont, OH", "Warren, OH", "Campbell, KY", "Boone, KY", "Kenton, KY"})
+        Me.cboCountyState.Location = New System.Drawing.Point(223, 70)
+        Me.cboCountyState.Name = "cboCountyState"
+        Me.cboCountyState.Size = New System.Drawing.Size(186, 24)
+        Me.cboCountyState.TabIndex = 0
+        '
+        'txtYearlyIncome
+        '
+        Me.txtYearlyIncome.Location = New System.Drawing.Point(223, 119)
+        Me.txtYearlyIncome.Name = "txtYearlyIncome"
+        Me.txtYearlyIncome.Size = New System.Drawing.Size(186, 22)
+        Me.txtYearlyIncome.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(104, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Select County:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(64, 122)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(139, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Enter Yearly Income:"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(49, 225)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(84, 45)
+        Me.btnSubmit.TabIndex = 4
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnTotalSurveyed
+        '
+        Me.btnTotalSurveyed.Location = New System.Drawing.Point(151, 225)
+        Me.btnTotalSurveyed.Name = "btnTotalSurveyed"
+        Me.btnTotalSurveyed.Size = New System.Drawing.Size(84, 45)
+        Me.btnTotalSurveyed.TabIndex = 5
+        Me.btnTotalSurveyed.Text = "Total Surveyed"
+        Me.btnTotalSurveyed.UseVisualStyleBackColor = True
+        '
+        'btnAverageIncome
+        '
+        Me.btnAverageIncome.Location = New System.Drawing.Point(253, 225)
+        Me.btnAverageIncome.Name = "btnAverageIncome"
+        Me.btnAverageIncome.Size = New System.Drawing.Size(84, 45)
+        Me.btnAverageIncome.TabIndex = 6
+        Me.btnAverageIncome.Text = "Average Income"
+        Me.btnAverageIncome.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(355, 225)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(84, 45)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmHouseholdIncome
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(501, 342)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnAverageIncome)
+        Me.Controls.Add(Me.btnTotalSurveyed)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtYearlyIncome)
+        Me.Controls.Add(Me.cboCountyState)
+        Me.Name = "frmHouseholdIncome"
+        Me.Text = "Household Income Survey"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cboCountyState As ComboBox
+    Friend WithEvents txtYearlyIncome As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnTotalSurveyed As Button
+    Friend WithEvents btnAverageIncome As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ frmSurvey.vb
@@ -0,0 +1,230 @@
+﻿
+Option Strict On
+'*********************************************
+' Neina Cichon
+' 2020-07-05
+' IT 102-401  Programming 2
+' Assignment 7 - Household Income Survey
+'*********************************************
+
+
+Public Class frmHouseholdIncome
+
+    'Declare Class Variables
+    Dim strOhio() As String = {"Hamilton", "Butler", "Clermont", "Warren"}
+    Dim strKentucky() As String = {"Campbell", "Boone", "Kenton"}
+    Dim intOhioCount(3) As Integer
+    Dim intKentuckyCount(2) As Integer
+    Dim dblIncomeOhio(3) As Double
+    Dim dblIncomeKentucky(2) As Double
+    Dim dblAveragesOH(3) As Double
+    Dim dblAveragesKY(2) As Double
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'Closes the program
+        Close()
+
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        'Declare Variables
+        Dim dblIncomeInput As Double
+
+        'Set backcolors back to white in case of error
+        cboCountyState.BackColor = Color.White
+        txtYearlyIncome.BackColor = Color.White
+
+        If ValidateInput(cboCountyState, dblIncomeInput) = True Then
+            dblIncomeInput = CDbl(txtYearlyIncome.Text)
+            GetCountSort(cboCountyState)
+            GetIncomeSort(cboCountyState, dblIncomeInput)
+            GetAverageSort()
+
+            'Reset Form after Submit click
+            cboCountyState.SelectedIndex = -1
+            txtYearlyIncome.Clear()
+        End If
+
+    End Sub
+
+
+    Function ValidateInput(ByRef County As ComboBox, ByRef Income As Double) As Boolean
+
+        'Validate all input
+        If ValidateCounty(County) = True Then
+            If ValidateIncome(Income) = True Then
+                Return True
+            Else
+                Return False
+            End If
+        Else
+            Return False
+        End If
+
+    End Function
+
+    Function ValidateCounty(ByRef County As ComboBox) As Boolean
+
+        'Checking to make sure County has been selected
+        If cboCountyState.SelectedIndex = -1 Then
+            MessageBox.Show("Please select County.")
+            cboCountyState.BackColor = Color.Yellow   'Set backcolor to yellow
+            cboCountyState.Focus()  'set focus back to errored cell for user
+            Return False
+        Else
+            Return True
+        End If
+
+    End Function
+
+    'Create Function to Validate Income
+    Function ValidateIncome(ByRef Income As Double) As Boolean
+
+        'Check Income to make sure it is Numeric
+        If IsNumeric(txtYearlyIncome.Text) Then
+            Income = CDbl(txtYearlyIncome.Text)
+
+            'Validate that Income is not negative
+            If Income >= 0 Then
+                Return True
+            Else
+                'Display error if negative
+                MessageBox.Show("Income can not be negative.")
+                txtYearlyIncome.BackColor = Color.Yellow     'Set color to yellow
+                txtYearlyIncome.Focus()    'set focus back to errored cell for user
+                Return False
+            End If
+        Else
+            'Display Error if Not Numeric
+            MessageBox.Show("Please enter Income")
+            txtYearlyIncome.BackColor = Color.Yellow     'Set color to yellow
+            txtYearlyIncome.Focus()    'set focus back to errored cell for user
+            Return False
+        End If
+
+    End Function
+
+    'Sub to Sort inputs by State and get count and put in array
+    Private Sub GetCountSort(ByVal CountyState As ComboBox)
+
+        'Declare Variables
+        Dim i As Integer
+        Dim County As String
+
+        If cboCountyState.Text.EndsWith("OH") Then
+            County = cboCountyState.Text.Replace(", OH", "")
+            i = strOhio.ToList.IndexOf(County)
+            intOhioCount(i) += 1
+
+        ElseIf cboCountyState.Text.EndsWith("KY") Then
+            County = cboCountyState.Text.Replace(", KY", "")
+            i = strKentucky.ToList.IndexOf(County)
+            intKentuckyCount(i) += 1
+        End If
+
+    End Sub
+
+    'Get the averages by county and put in Array
+    Private Sub GetAverageSort()
+
+        'Declare Variables
+        Dim i As Integer
+        Dim ik As Integer
+
+        'Get Ohio Averages
+        Try
+            For Each average As Double In dblAveragesOH
+                If (intOhioCount(i) <= 0) Then 'Make sure not dividing by 0
+                    dblAveragesOH(i) = 0
+                Else
+                    dblAveragesOH(i) = CDbl(dblIncomeOhio(i) / intOhioCount(i))
+
+                End If
+                i += 1
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+        'Get Kentucky Averages
+        Try
+            For Each average As Double In dblAveragesKY
+                If (intKentuckyCount(ik) <= 0) Then 'Make sure not dividing by 0
+                    dblAveragesKY(ik) = 0
+                Else
+                    dblAveragesKY(ik) = CDbl(dblIncomeKentucky(ik) / intKentuckyCount(ik))
+
+                End If
+                ik += 1
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+
+
+    End Sub
+
+    'Sort By state and put into array by county
+    Private Sub GetIncomeSort(ByVal CountyState As ComboBox, ByVal Income As Double)
+
+        'Declare Variables
+        Dim intIndex As Integer
+
+        If cboCountyState.Text.EndsWith("OH") Then
+            intIndex = strOhio.ToList.IndexOf(cboCountyState.Text.Replace(", OH", ""))
+            dblIncomeOhio(intIndex) += Income
+        Else
+            intIndex = strKentucky.ToList.IndexOf(cboCountyState.Text.Replace(", KY", ""))
+            dblIncomeKentucky(intIndex) += Income
+        End If
+
+    End Sub
+
+    Private Sub btnTotalSurveyed_Click(sender As Object, e As EventArgs) Handles btnTotalSurveyed.Click
+
+        'Display Counts of surveyed households in MsgBox
+        MessageBox.Show("Ohio: " & vbTab & vbTab & vbTab & intOhioCount.Sum & vbNewLine & vbTab &
+             "Hamilton: " & vbTab & intOhioCount(0) & vbNewLine & vbTab &
+            "Butler: " & vbTab & vbTab & intOhioCount(1) & vbNewLine & vbTab &
+            "Clermont: " & vbTab & intOhioCount(2) & vbNewLine & vbTab &
+            "Warren: " & vbTab & vbTab & intOhioCount(3) & vbNewLine &
+            "Kentucky: " & vbTab & vbTab & intKentuckyCount.Sum & vbNewLine & vbTab &
+            "Boone: " & vbTab & vbTab & intKentuckyCount(0) & vbNewLine & vbTab &
+            "Campbell: " & vbTab & intKentuckyCount(1) & vbNewLine & vbTab &
+            "Kenton: " & vbTab & vbTab & intKentuckyCount(2), "Number of Households Surveyed")
+
+    End Sub
+
+    Private Sub btnAverageIncome_Click(sender As Object, e As EventArgs) Handles btnAverageIncome.Click
+
+        'Declare Variables
+        Dim dblAverageOhio As Double
+        Dim dblAverageKY As Double
+        Dim incomeAllOhio = Aggregate income In dblIncomeOhio Into Sum()
+        Dim countAllOhio = Aggregate count In intOhioCount Into Sum()
+
+        'Get state averages
+        If intOhioCount.Sum > 0 Then
+            dblAverageOhio = CDbl(incomeAllOhio / countAllOhio)
+        End If
+        If intKentuckyCount.Sum > 0 Then
+            dblAverageKY = CDbl(dblIncomeKentucky.Sum / intKentuckyCount.Sum)
+        End If
+
+        'Display messagebox with Totals
+        MessageBox.Show("Ohio: " & vbTab & vbTab & vbTab & FormatCurrency(dblAverageOhio) & vbNewLine & vbTab &
+         "Hamilton: " & vbTab & FormatCurrency(dblAveragesOH(0)) & vbNewLine & vbTab &
+         "Butler: " & vbTab & vbTab & FormatCurrency(dblAveragesOH(1)) & vbNewLine & vbTab &
+         "Clermont: " & vbTab & FormatCurrency(dblAveragesOH(2)) & vbNewLine & vbTab &
+         "Warren: " & vbTab & vbTab & FormatCurrency(dblAveragesOH(3)) & vbNewLine &
+         "Kentucky: " & vbTab & vbTab & FormatCurrency(dblAverageKY) & vbNewLine & vbTab &
+         "Boone: " & vbTab & vbTab & FormatCurrency(dblAveragesKY(0)) & vbNewLine & vbTab &
+         "Campbell: " & vbTab & FormatCurrency(dblAveragesKY(1)) & vbNewLine & vbTab &
+         "Kenton: " & vbTab & vbTab & FormatCurrency(dblAveragesKY(2)), "Average Household Income")
+
+    End Sub
+End Class
